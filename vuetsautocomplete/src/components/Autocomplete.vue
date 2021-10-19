e6c772804ca52ad1922f13933e5305cda3110f21
ffullex <tomanovskiyyv@mer.ci.nsu.ru>
2021-10-19 14:23:26 +0700

    checked api

--- vuetsautocomplete/src/components/Autocomplete.vue
+++ vuetsautocomplete/src/components/Autocomplete.vue
@@ -1,27 +1,38 @@
 <template>
   <div>
     <select>
-      <option v-for="item in citiesList" :key="item.id">
+      <option v-for="item in people" :key="item.id">
         {{ item.name }}
       </option>
     </select>
+    <button @click="getApiInfo">Получить список</button>
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
+import { defineComponent } from "vue";
 
-export default defineComponent( {
+export default defineComponent({
   name: "Autocomplete",
   data() {
     return {
-    citiesList: [
-      {id:1, name: "a"},
-      {id:2, name: "b"},
-      {id:3, name: "c"},
-    ]
-  }},
+      citiesList: [
+        {id: 1, name: "a"},
+        {id: 2, name: "b"},
+        {id: 3, name: "c"},
+      ],
+      people: []
+    }
+  },
+  methods: {
+    async getApiInfo() {
+      const appi = await fetch("https://jsonplaceholder.typicode.com/users")
+      this.people = await appi.json();
+      console.log(this.people)
+    }
+  }
 });
+
 </script>
 
 <style scoped>
